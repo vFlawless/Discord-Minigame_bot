e9850fc2fd686194d5ffcf955efccbbb7126db70
vFlawless <68557069+vFlawless@users.noreply.github.com>
2022-03-14 13:06:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wordle_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
